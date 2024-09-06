04a314633597d9616bced60237c6678aebc1ae0a
Annie Xing <mxxing@live.com>
2024-09-06 08:07:03 +0200

    fix issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: "export",
     distDir: "dist",
     images:{
+        unoptimized: true,
         remotePatterns:[
             {
                 protocol: 'https',
